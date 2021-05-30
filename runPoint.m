195152bca585a3a1b79a274c404b4110e5d931f2
saeedafra <saeed.afrasiabi@gmail.com>
2021-05-30 21:31:59 +0200

    missed corrections

--- runPoint.m
+++ runPoint.m
@@ -1,14 +1,22 @@
 %this script is a template for runnig the simulator
 setPath
 
-%results
+inStruct.SNRdb=-10:5:15;
+inStruct.modulationOrder=16;
+inStruct.numBits=1e6;
+inStruct.noisePower=0.01;
+
+outStruct=runSim(inStruct);
+
 figure
-snrlin=10.^(inStruct.SNRdb/10);
-semilogy(inStruct.SNRdb,SER,'r*')
-Theory=theory(inStruct.modulationOrder,snrlin);
+snrLin=10.^(inStruct.SNRdb/10);
+semilogy(inStruct.SNRdb,outStruct.SER,'r*')
+
+analyticSER=theory(inStruct.modulationOrder,snrLin);
 hold on
-semilogy(inStruct.SNRdb,Theory,'b.-')
+semilogy(inStruct.SNRdb,analyticSER,'b.-')
+
 xlabel('SNR [dB]')
 ylabel('SER [dB]')
 grid on
-title(['Modulation' Modulation(log2(inStruct.modulationOrder))])
+title(sprintf('modOrder=%d',inStruct.modulationOrder))
