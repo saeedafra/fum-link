ff9aa565b7f0dfe19a607ebadfe83ce6550c840f
saeedafra <saeed.afrasiabi@gmail.com>
2021-06-14 16:21:33 +0200

    Update test1_yxw.m

--- test_scenarios/test1_yxw.m
+++ test_scenarios/test1_yxw.m
@@ -2,6 +2,7 @@ inStruct.SNRdb=-10:5:15;
 inStruct.modulationOrder=16;
 inStruct.numBits=1e6;
 inStruct.noisePower=0.01;
+%turn off all the rest
 
 %saeed: these internal variables must be kept internal. either turn it into a
 %function or what?
